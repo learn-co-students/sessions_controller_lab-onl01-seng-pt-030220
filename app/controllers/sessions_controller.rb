023484a0016dba2b7d171010fcc935020c24d758
engahmed10 <eng.computer1@yahoo.com>
2020-07-23 00:36:28 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,28 +6,20 @@ class SessionsController < ApplicationController
     end
 
     def  create
-       # byebug
-       # if session[:name] == params[:name]
-        #    redirect_to '/'
-       # end
-    # if !!session[:name]
-     #   redirect_to '/'
-    # else
-        if params[:name] == '' || params[:name] == nil
+      
+        if !params[:name].present?
             redirect_to login_path
         else
-       
-       session[:name] = params[:name]
-        
+           session[:name] = params[:name]
         end
-     if !!session[:name]
-        redirect_to '/'
-    end
-  
+        if !!session[:name]
+            redirect_to '/'
+        end
+
     end
 
     def destroy 
-        session.delete :name if session[:name]
+        session.delete :name 
     end
 
 end
